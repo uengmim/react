3b4a184a2fee9312ca114fa4cb47e3e501a350a0
채승민 <72143238+yalmipzi@users.noreply.github.com>
2022-05-02 12:32:07 +0900

    Project done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className='title'>리액트 시작</h1>
+        <h1> 첫 시간</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
